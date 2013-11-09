b73e7daeb24c2df633a23a4f640ce8b94eacdd57
Mathias <matsou03@gmail.com>
2013-11-09 14:25:11 +0100

    device storage

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var deviceStoragePictures = document.querySelector("#device-storage-pictures"),
-        deviceStoragePicturesDisplay = document.querySelector("#device-storage-pictures-display");
+var deviceStoragePictures = document.querySelector("device-storage-pictures"),
+        deviceStoragePicturesDisplay =  document.querySelector("device-storage-pictures-display");
     if (deviceStoragePictures && deviceStoragePicturesDisplay) {
         deviceStoragePictures.onclick = function () {
+
             var deviceStorage = navigator.getDeviceStorage("pictures"),
-                cursor = deviceStorage.enumerate(); 
+                cursor = deviceStorage.enumerate();
 
             deviceStoragePicturesDisplay.innerHTML = "<h4>Result from deviceStorage - pictures</h4>";
- 
-            cursor.onsuccess = function () { 
-                if (!cursor.result)  {
+            cursor.onsuccess = function () {
+                if (!cursor.result) {
                     deviceStoragePicturesDisplay.innerHTML = "No files";
                 }
 
                 var file = cursor.result,
-                    filePresentation; 
+                    filePresentation;
 
                 filePresentation = "<strong>" + file.name + ":</strong> " + parseInt(file.size / 1024, 10) + "kb<br>";
                 filePresentation += "<p><img src='" + window.URL.createObjectURL(file) + "' alt=''></p>";
@@ -22,7 +22,7 @@ var deviceStoragePictures = document.querySelector("#device-storage-pictures"),
                 deviceStoragePicturesDisplay.style.display = "block";
             };
 
-              cursor.onerror = function () {
+                cursor.onerror = function () {
                 console.log("Error");
                 deviceStoragePicturesDisplay.innerHTML = "<h4>Result from deviceStorage - pictures</h4><p>deviceStorage failed</p>";
                 deviceStoragePicturesDisplay.style.display = "block";
