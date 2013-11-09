952dadccfcd899f72c74c51dc1ad9df084366e49
Mathias <matsou03@gmail.com>
2013-11-09 17:07:09 +0100

    Ajout de la liste des photos

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-var deviceStoragePictures = document.querySelector("#device-storage-pictures"),
-        deviceStoragePicturesDisplay =  document.querySelector("#device-storage-pictures-display");
-    if (deviceStoragePictures && deviceStoragePicturesDisplay) {
-        deviceStoragePictures.onclick = function () {
-
-            var deviceStorage = navigator.getDeviceStorage("pictures"),
-                cursor = deviceStorage.enumerate();
-
-            deviceStoragePicturesDisplay.innerHTML = "<h4>Result from deviceStorage - pictures</h4>";
-            cursor.onsuccess = function () {
-                if (!cursor.result) {
-                    deviceStoragePicturesDisplay.innerHTML = "No files";
-                }
-
-                var file = cursor.result,
-                    filePresentation;
-
-                filePresentation = "<strong>" + file.name + ":</strong> " + parseInt(file.size / 1024, 10) + "kb<br>";
-                filePresentation += "<p><img src='" + window.URL.createObjectURL(file) + "' alt=''></p>";
-                deviceStoragePicturesDisplay.innerHTML += filePresentation;
-
-                deviceStoragePicturesDisplay.style.display = "block";
-            };
-
-                cursor.onerror = function () {
-                console.log("Error");
-                deviceStoragePicturesDisplay.innerHTML = "<h4>Result from deviceStorage - pictures</h4><p>deviceStorage failed</p>";
-                deviceStoragePicturesDisplay.style.display = "block";
-            };
-        };
+var deviceStoragePicturesDisplay =  document.querySelector("#device-storage-pictures-display");
+    if (deviceStoragePicturesDisplay) {
+        showGallery();
     }
+
+function showGallery() {
+
+    var deviceStorage = navigator.getDeviceStorage("pictures"),
+        cursor = deviceStorage.enumerate();
+
+    deviceStoragePicturesDisplay.innerHTML = "";
+    cursor.onsuccess = function () {
+
+
+        var filePresentation = "<p id='gallery'>";
+        
+        var idImage = 0;
+        var images = new Array;
+
+        while (cursor.result) {
+            var file = cursor.result;
+
+            filePresentation += "<img class='image' id='image"+idImage+"' src='" + window.URL.createObjectURL(file) + "' alt=''>";
+            deviceStoragePicturesDisplay.innerHTML += filePresentation;
+
+            
+            images[idImage] =  document.querySelector("#image"+idImage);
+            images[idImage].onclick = function () {
+                showImage(idImage);
+            };
+
+            idImage++;
+            cursor.continue();
+        }
+
+        filePresentation += "/<p>";
+    };
+
+    cursor.onerror = function () {
+        console.log("Error");
+        deviceStoragePicturesDisplay.innerHTML = "<h4>Result from deviceStorage - pictures</h4><p>deviceStorage failed</p>";
+        deviceStoragePicturesDisplay.style.display = "block";
+    }
+}
+
+
+function showImage(id) {
+    alert("show "+id);
+}
+
